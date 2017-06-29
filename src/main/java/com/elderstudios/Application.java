576640848da50e39fd8c3f3980f599f2400a055c
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-06-29 16:34:23 +0100

    Initial Config Class.

---
+++ src/main/java/com/elderstudios/Application.java
@@ -0,0 +1,16 @@
+package com.elderstudios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tony on 29/06/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main (String args []) throws Exception {
+        SpringApplication.run (Application.class, args);
+    }
+}
