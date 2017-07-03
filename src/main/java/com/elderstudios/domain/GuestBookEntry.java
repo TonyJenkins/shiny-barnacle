a5163009e9bdb62e95983884152e8327e5156dc4
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-07-03 11:39:41 +0100

    Add creation and last update fields to domain and creation script.

--- src/main/java/com/elderstudios/domain/GuestBookEntry.java
+++ src/main/java/com/elderstudios/domain/GuestBookEntry.java
@@ -1,8 +1,11 @@
 package com.elderstudios.domain;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by tony on 29/06/17.
@@ -23,12 +26,27 @@ public class GuestBookEntry {
     @NotEmpty
     private String comment;
 
-    public GuestBookEntry() {
+    @CreatedDate
+    @Column (name = "created_date")
+    private Date created;
+
+    @LastModifiedDate
+    @Column (name = "modified_date")
+    private Date lastUpdated;
+
+    public GuestBookEntry () {
     }
 
-    public GuestBookEntry(String user, String comment) {
+    public GuestBookEntry (String user, String comment) {
         this.user = user;
         this.comment = comment;
+    }
+
+    public GuestBookEntry (String user, String comment, Date created, Date lastUpdated) {
+        this.user = user;
+        this.comment = comment;
+        this.created = created;
+        this.lastUpdated = lastUpdated;
     }
 
     public Integer getId() {
@@ -55,12 +73,31 @@ public class GuestBookEntry {
         this.comment = comment;
     }
 
+    public Date getCreated () {
+        return created;
+    }
+
+    public void setCreated (Date created) {
+        this.created = created;
+    }
+
+    public Date getLastUpdated () {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated (Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
     @Override
     public String toString () {
         return "GuestBookEntry{" +
                 "id=" + id +
                 ", user='" + user + '\'' +
                 ", comment='" + comment + '\'' +
+                ", created=" + created +
+                ", lastUpdated=" + lastUpdated +
                 '}';
     }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
-insert into entries (user, comment) values ('Dilbert', 'Hello, World.');
-insert into entries (user, comment) values ('Wally', 'The Geese Fly South.');
+insert into entries (user, comment, created_date, modified_date)
+            values ('Dilbert', 'Hello, World.', now(), now());
+insert into entries (user, comment, created_date, modified_date)
+            values ('Wally', 'The Geese Fly South.', now(), now());
